77e1940eed685f318bb5fff75184cbf3117402fa
noitcelfer7 <noitcelfer7@gmail.com>
2024-10-15 14:07:23 +0500

    Исправил ошибку

--- apps/players/src/player/player.module.ts
+++ apps/players/src/player/player.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlayerEntity } from './entities';
 import { PlayerService } from './services/player.service';
-import { PlayerController } from './controllers';
 
 @Module({
-  controllers: [PlayerController],
   imports: [TypeOrmModule.forFeature([PlayerEntity])],
   exports: [PlayerService],
   providers: [PlayerService],
